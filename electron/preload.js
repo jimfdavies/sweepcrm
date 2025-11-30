bb1953d44bfbf726d71395186db354270a7e6fc5
jimfdavies <3219954+jimfdavies@users.noreply.github.com>
2025-11-30 20:22:00 +0000

    reset

--- electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  // Add secure IPC methods here
-  ping: () => ipcRenderer.invoke('ping'),
-});
